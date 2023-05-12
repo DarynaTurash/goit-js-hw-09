7e28cdb120867ef301bdf7a543f83b17867a3383
DarynaTurash <daryna.turash@gmail.com>
2023-05-13 01:46:09 +0300

    color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+};
+
+let timerId = null;
+let currentColor = '';
+refs.btnStop.setAttribute('disabled', 'disabled');
+
+
+refs.btnStart.addEventListener('click', onChangeColor);
+refs.btnStop.addEventListener('click', onStopChanging);
+
+function onChangeColor() {
+    timerId = setInterval(() => {
+        currentColor = getRandomHexColor();
+        refs.body.style.backgroundColor = currentColor;
+        refs.btnStart.setAttribute('disabled', 'disabled');
+        refs.btnStop.removeAttribute('disabled');
+    }, 1000);
+};
+
+function onStopChanging() {
+    clearInterval(timerId);
+    refs.btnStart.removeAttribute('disabled');
+    refs.btnStop.setAttribute('disabled', 'disabled');
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
 
